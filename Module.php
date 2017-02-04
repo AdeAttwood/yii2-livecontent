46e327abd6e26ff9999d5fc87a6078c233175ffe
Ade Attwood <attwood16@googlemail.com>
2017-02-04 09:37:39 +0000

    Update Module.php

--- Module.php
+++ Module.php
@@ -90,8 +90,9 @@ class Module extends \yii\base\Module implements BootstrapInterface
     public function renderEditDialog($event){
 		if ($this->_editDialogRender){
 	    	echo Yii::$app->getView()->render(
-	    		'@vendor/sergeykoz/yii2-livecontent/views/dialog'          
-	    	);
+	    		//'@vendor/sergeykoz/yii2-livecontent/views/dialog'          
+	    		'@vendor/adeattwood/yii2-livecontent/views/dialog'
+		);
     	}
     }
 }
